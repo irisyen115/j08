c21d0b94000a15d38aaf01ea8f6cda269ecccf1b
irisyen115 <110b16890@mailst.cjcu.edu.tw>
2022-11-21 22:15:39 +0800

    Change to traffic lights

--- java12/class14/class14.java
+++ java12/class14/class14.java
@@ -4,6 +4,11 @@ public class class14 {
     public static void main(String[] args) {
         String s = "Habit is second nature.";
         // 如果nature不在s裡面，indexOf就會回傳-1，而-1無法substring任何東西
-        System.out.println("s.substring(s.indexOf(\"nature\"))=" + s.substring(s.indexOf("nature")));
+        int a = s.indexOf("nature");
+        if (a == -1) {
+            System.out.println("not in s");
+        } else {
+            System.out.println("s.substring(s.indexOf(\"nature\"))=" + s.substring(a));
+        }
     }
 }
