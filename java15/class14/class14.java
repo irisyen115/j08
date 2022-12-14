2eff860e6d8a73eca24ef9fb8f657ee032af6bf1
irisyen115 <110b16890@mailst.cjcu.edu.tw>
2022-12-14 22:32:48 +0800

    #20 add Phone class and explain Synchronize problem

--- java15/class14/class14.java
+++ java15/class14/class14.java
@@ -1,32 +1,54 @@
 package class14;
 
-class PrePaid extends Thread {
-    private int sum = 200;
+class PrePaid {
+    public int sum = 200;
 
     public void talk(int n) {
-        sum -= n;
+        int tmp = sum;
         try {
             Thread.sleep((int) (1000 * Math.random()));
         } catch (InterruptedException e) {
         }
-        System.out.println("sum= " + sum);
+        sum = tmp - n;
+        show(n);
+    }
+    // 這樣子三個電話亭沒辦法同步扣款，會出現錯誤
+
+    public synchronized void show(int n) {
+        if (sum > 10) {
+            System.out.println("通話費:" + n);
+            System.out.println("sum= " + sum);
+        }
     }
 
+}
+
+class Phone extends Thread {
+    PrePaid card;
+
+    Phone(PrePaid _card) {
+        card = _card;
+    }
+
+    int fee = (int) ((Math.random()) * 100);
+
     public void run() {
-        int fee = (int) ((Math.random()) * 99);
-        if (sum >= 10) {
-            talk(fee);
+        while (card.sum > 10) {
+            card.talk(fee);
         }
     }
 }
 
 public class class14 {
     public static void main(String[] args) {
-        PrePaid card1 = new PrePaid();
-        PrePaid card2 = new PrePaid();
-        PrePaid card3 = new PrePaid();
-        card1.start();
-        card2.start();
-        card3.start();
+        PrePaid card = new PrePaid();
+        Phone[] phones = new Phone[3];
+        for (int i = 0; i < phones.length; i++) {
+            phones[i] = new Phone(card);
+        }
+        for (int i = 0; i < phones.length; i++) {
+            phones[i].start();
+        }
+
     }
 }
