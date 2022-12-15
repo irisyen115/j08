b2544d6d5356424d5bd36c4b46a3e41560035956
irisyen115 <98208602+irisyen115@users.noreply.github.com>
2022-12-15 22:39:34 +0800

    Add files via upload

---
+++ java15/class14.java
@@ -0,0 +1,32 @@
+package class14;
+
+class PrePaid extends Thread {
+    private int sum = 200;
+
+    public void talk(int n) {
+        sum -= n;
+        try {
+            Thread.sleep((int) (1000 * Math.random()));
+        } catch (InterruptedException e) {
+        }
+        System.out.println("sum= " + sum);
+    }
+
+    public void run() {
+        int fee = (int) ((Math.random()) * 99);
+        if (sum >= 10) {
+            talk(fee);
+        }
+    }
+}
+
+public class class14 {
+    public static void main(String[] args) {
+        PrePaid card1 = new PrePaid();
+        PrePaid card2 = new PrePaid();
+        PrePaid card3 = new PrePaid();
+        card1.start();
+        card2.start();
+        card3.start();
+    }
+}
