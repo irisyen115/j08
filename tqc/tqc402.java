3b711b0cb3e3c2987ec5e129720e92e91bf166b5
irisyen115 <110b16890@mailst.cjcu.edu.tw>
2022-12-26 21:50:23 +0800

    finish 402

---
+++ tqc/tqc402.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class tqc402 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 0;
+        while (n >= 0) {
+            System.out.println("Input n (0 <= n <= 16):");
+            n = sc.nextInt();
+            if (n == 999) {
+                break;
+            }
+            int count = 1;
+            for (int i = 1; i < n + 1; i++) {
+                count *= i;
+            }
+            System.out.println(n + "的階乘<尾端遞迴> = " + lad(n));
+            System.out.println(n + "的階乘<迴圈> = " + count);
+        }
+        sc.close();
+    }
+
+    public static int lad(int n) {
+        if (n == 1) {
+            return 1;
+        } else {
+            return n * lad(n - 1);
+        }
+    }
+}
