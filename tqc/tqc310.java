40a9a8e849b5a094fc4214fd7ff3cd4dbe75e0d3
irisyen115 <110b16890@mailst.cjcu.edu.tw>
2022-12-26 21:28:50 +0800

    finish 310

---
+++ tqc/tqc310.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class tqc310 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = 0;
+        do {
+            System.out.println("請輸入n的值(n > 0，且為偶數)");
+            n = sc.nextInt();
+        } while (n % 2 != 0);
+        System.out.println("2 + 4 + 6 + ... + n = " + ((2 + n) * (n / 2)) / 2);
+        sc.close();
+    }
+}
