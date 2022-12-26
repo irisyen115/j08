a96a3dc2a0d587103eadc32604a8b323b490e30b
irisyen115 <110b16890@mailst.cjcu.edu.tw>
2022-12-26 22:13:26 +0800

    #23 Change the order of judgment

--- tqc/tqc208.java
+++ tqc/tqc208.java
@@ -6,16 +6,16 @@ public class tqc208 {
         for (int i = 0; i < 5; i++) {
             System.out.println("Input:");
             int a = sc.nextInt();
-            if (a >= 90) {
-                System.out.println("Your grade is A");
-            } else if (a < 90 && a >= 80) {
-                System.out.println("Your grade is B");
-            } else if (a < 80 && a >= 70) {
-                System.out.println("Your grade is C");
+            if (a < 60) {
+                System.out.println("Your grade is F");
             } else if (a < 70 && a >= 60) {
                 System.out.println("Your grade is D");
-            } else if (a < 60) {
-                System.out.println("Your grade is F");
+            } else if (a < 80 && a >= 70) {
+                System.out.println("Your grade is C");
+            } else if (a < 90 && a >= 80) {
+                System.out.println("Your grade is B");
+            } else if (a >= 90) {
+                System.out.println("Your grade is A");
             }
         }
         sc.close();
