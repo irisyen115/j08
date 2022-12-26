f4fafcca46322b36aefc1de16373bc1abfcf696a
irisyen115 <110b16890@mailst.cjcu.edu.tw>
2022-12-26 23:07:29 +0800

    #23 Adjustment judgment

--- tqc/tqc208.java
+++ tqc/tqc208.java
@@ -8,11 +8,11 @@ public class tqc208 {
             int a = sc.nextInt();
             if (a < 60) {
                 System.out.println("Your grade is F");
-            } else if (a >= 60 && a < 70) {
+            } else if (a < 70) {
                 System.out.println("Your grade is D");
-            } else if (a >= 70 && a < 80) {
+            } else if (a < 80) {
                 System.out.println("Your grade is C");
-            } else if (a >= 80 && a < 90) {
+            } else if (a < 90) {
                 System.out.println("Your grade is B");
             } else {
                 System.out.println("Your grade is A");
