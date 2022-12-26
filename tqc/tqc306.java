0727fdf201c4dcf271bdafafa20ee89f931a26fe
irisyen115 <110b16890@mailst.cjcu.edu.tw>
2022-12-26 21:17:02 +0800

    finish 306

---
+++ tqc/tqc306.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class tqc306 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
+        String[] t;
+        int m = 0;
+        int n = 0;
+        while (m >= 0) {
+            System.out.println("Input:");
+            t = bf.readLine().split(" ");
+            m = Integer.parseInt(t[0]);
+            n = Integer.parseInt(t[1]);
+            if (m == 999) {
+                break;
+            }
+            System.out.println((int) (Math.pow(m, n)));
+        }
+    }
+}
