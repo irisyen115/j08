8644f13fea853ba7323844d888ae9a6aaceefe33
irisyen115 <110b16890@mailst.cjcu.edu.tw>
2022-12-27 17:45:14 +0800

    change for loop

--- tqc/tqc506.java
+++ tqc/tqc506.java
@@ -3,10 +3,10 @@ public class tqc506 {
         int[][][] A = { { { 1, 2, 3 }, { 4, 5, 6 } }, { { 7, 8, 9 }, { 10, 11, 12 } },
                 { { 13, 14, 15 }, { 16, 17, 18 } }, { { 19, 20, 21 }, { 22, 23, 24 } } };
         int sum = 0;
-        for (int i = 0; i < A.length; i++) {
-            for (int j = 0; j < A[i].length; j++) {
-                for (int k = 0; k < A[i][j].length; k++) {
-                    sum += A[i][j][k];
+        for (int[][] i : A) {
+            for (int[] j : i) {
+                for (int v : j) {
+                    sum += v;
                 }
             }
         }
