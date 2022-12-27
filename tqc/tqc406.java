9b3f99aa580009f4896e16bf500a07c434008608
irisyen115 <110b16890@mailst.cjcu.edu.tw>
2022-12-27 17:18:04 +0800

    finish 406

---
+++ tqc/tqc406.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class tqc406 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        for (int i = 0; i < 2; i++) {
+            System.out.print("Input a string: ");
+            String s = sc.nextLine();
+            System.out.println(s + " has " + findA(s) + " As");
+        }
+    }
+
+    public static int findA(String s) {
+        if (s.length() == 0) {
+            return 0;
+        } else {
+            return ((s.charAt(0) == 'A') ? 1 : 0) + (findA(s.substring(1, s.length())));
+        }
+    }
+}
