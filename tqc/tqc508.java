a9af25d018dec89dfa70a753ea59b9aa17f582f4
irisyen115 <110b16890@mailst.cjcu.edu.tw>
2022-12-27 18:19:20 +0800

    change loop range

--- tqc/tqc508.java
+++ tqc/tqc508.java
@@ -1,8 +1,8 @@
 public class tqc508 {
     public static void main(String[] args) {
         int[] a = { 2, 4, 3, 5, 7, 6, 9, 1 };
-        for (int i = 0; i < a.length; i++) {
-            for (int j = 0; j < a.length - 1; j++) {
+        for (int i = 0; i < a.length - 1; i++) {
+            for (int j = 0; j < a.length - 1 - i; j++) {
                 if (a[j] > a[j + 1]) {
                     int tmp = a[j];
                     a[j] = a[j + 1];
